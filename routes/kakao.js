f7056f9c0bd9a0fb5715c43faa05c282c1fb25b0
박시현 <f22raptor_sh@naver.com>
2024-08-22 08:24:00 +0900

    modified:   routes/kakao.js

--- routes/kakao.js
+++ routes/kakao.js
@@ -1,10 +1,9 @@
 var express = require("express");
 var router = express.Router();
 const xlsx = require("xlsx");
+const fs = require("fs");
 
 let db; // db 객체를 받기 위한 변수 선언
-
-const filePath = "/views/user.xlsx";
 
 router.setDb = (database) => {
   db = database; // db 객체를 저장
@@ -15,17 +14,102 @@ const userlogPath = "./views/user.xlsx";
 router.post("/", function (req, res, next) {
   let method = req.get("method");
   let user_id = req.body["userRequest"]["user"]["id"];
-  let std_num = req.body["userRequest"]["utterance"];
+  let user_input = req.body["userRequest"]["utterance"];
+
   // if (method == "user") {
-  //   // user_id가 B열에 이미 있는지, std_num이 A에 있고, 그 옆의 B가 비어있는지 확인 후
-  //   // B열에 없고 비어있다면 추가
-  //   // B열에 있고 std_num과 동일하면 이미 등록되었다는 안내
-  //   // B열에 있고 std_num과 다르면 이미 본 계정은 다른 학번으로 등록되었다는 안내
+  if (!fs.existsSync(userlogPath)) {
+    console.log("No Log file on path " + userlogPath);
+    return res
+      .status(404)
+      .json({ message: "사용자 로그 파일을 찾을 수 없습니다." });
+  }
+
+  const workbook = xlsx.readFile(userlogPath);
+  const sheet = workbook.Sheets[workbook.SheetNames[0]];
+  const data = xlsx.utils.sheet_to_json(sheet, { header: 1 });
+
+  let found = false;
+
+  for (let i = 0; i < data.length; i++) {
+    const [input, id] = data[i];
+
+    if (input === user_input) {
+      found = true;
+
+      if (id === user_id) {
+        return res
+          .json({
+            version: "2.0",
+            template: {
+              outputs: [
+                {
+                  simpleText: {
+                    text: "이전에 인증한 상태 입니다.",
+                  },
+                },
+              ],
+            },
+          })
+          .status(200);
+      } else if (!id) {
+        data[i][1] = user_id; // B열이 비어있다면 새로 등록
+        const updatedSheet = xlsx.utils.aoa_to_sheet(data);
+        workbook.Sheets[workbook.SheetNames[0]] = updatedSheet;
+        xlsx.writeFile(workbook, userlogPath);
+        return res
+          .json({
+            version: "2.0",
+            template: {
+              outputs: [
+                {
+                  simpleText: {
+                    text: "성공적으로 인증되었습니다.",
+                  },
+                },
+              ],
+            },
+          })
+          .status(200);
+      } else {
+        return res
+          .json({
+            version: "2.0",
+            template: {
+              outputs: [
+                {
+                  simpleText: {
+                    text: "다른 학번으로 이미 인증되어있습니다.",
+                  },
+                },
+              ],
+            },
+          })
+          .status(200);
+      }
+    }
+  }
+
+  if (!found) {
+    return res
+      .json({
+        version: "2.0",
+        template: {
+          outputs: [
+            {
+              simpleText: {
+                text: "존재하지 않는 학번입니다. 다시 확인해주세요.",
+              },
+            },
+          ],
+        },
+      })
+      .status(200);
+  }
   // } else if (True) {
   //   // 여기에는 사용자의 실제 id와 excel에 저장된 id가 일치하는지 확인한다.
   //   // 일치할 경우 다음을 실행한다.
   //   if (method == "check") {
-  //     let ref = db.ref("/" + std_num + "/");
+  //     let ref = db.ref("/" + user_input + "/");
   //     s_ref.once("value", (snapshot) => {
   //       const std_data = snapshot.val();
   //       // 이 부분을 챗봇의 응답 형식으로 바꿔야함. 깔끔하게 바꾸는게 좋을 듯
@@ -100,20 +184,6 @@ router.post("/", function (req, res, next) {
   //     })
   //     .status(200);
   // }
-  res
-    .json({
-      version: "2.0",
-      template: {
-        outputs: [
-          {
-            simpleText: {
-              text: "recive data" + method + "/" + user_id + "/" + std_num,
-            },
-          },
-        ],
-      },
-    })
-    .status(200);
 });
 
 module.exports = router;
