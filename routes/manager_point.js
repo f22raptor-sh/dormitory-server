2c8997fc504517336f1d25bbe0f95cab36efd7a4
박시현 <f22raptor_sh@naver.com>
2024-08-22 20:56:52 +0900

    finish v3

--- routes/manager_point.js
+++ routes/manager_point.js
@@ -290,9 +290,4 @@ router.post("/resetpw", function (req, res, next) {
   }
 });
 
-router.use(function (err, req, res, next) {
-  console.error("Internal Server Error:", err);
-  res.status(500).json({ error: "Internal Server Error" });
-});
-
 module.exports = router;
