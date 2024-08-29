00f0916967a1fa26c8ffb4d28adb2d338b673210
박시현 <f22raptor_sh@naver.com>
2024-08-29 19:22:42 +0900

    modified:   routes/manager_point.js

--- routes/manager_point.js
+++ routes/manager_point.js
@@ -297,6 +297,14 @@ router.post("/resetpw", function (req, res, next) {
 router.post("/resetterm", function (req, res, next) {
   let s_ref = db.ref("/");
   updates = {};
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, "0");
+  const date = String(today.getDate()).padStart(2, "0");
+  const hours = String(today.getHours()).padStart(2, "0");
+  const minutes = String(today.getMinutes()).padStart(2, "0");
+  const seconds = String(today.getSeconds()).padStart(2, "0");
+  const day = `${year}-${month}-${date}-${hours}:${minutes}:${seconds}`;
   s_ref.once("value").then((snapshot) => {
     for (std_number in snapshot.val()) {
       let s_ref = db.ref("/" + std_number + "/");
